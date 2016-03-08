9a1242cc88397e9c2383c3d04490ca3add5f8800
Jesse Shawl <jesse@jshawl.com>
2016-03-08 08:36:57 -0500

    update demo

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ var slipshod = require("./slipshod");
 
 slipshod.connect(function(bot){
   bot.on("dm", function(msg){
-    console.log("dmd", msg);
-  })
-  bot.on("mention", function(msg){
-
-    msg.reply(msg.text.split(" ").reverse().join(" "), function(response){
-      console.log(response)
-    })
+    console.log(msg)
   })
 })
